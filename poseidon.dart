bf96e446f3c8c6f007f5e5d14b80e94614d1e367
roseTech <110260001+roseTech@users.noreply.github.com>
2022-08-25 19:47:41 +0200

    First Commit

---
+++ poseidon.dart
@@ -0,0 +1,26 @@
+main() {
+  /*Poseidon’s Ice Cubes 🧊🧊🧊⚓️🧜‍♂️
+
+It’s boat day today with beautiful weather at the sea, 
+and all is packed to seize the day to the fullest, to ‘carpe diem’, 
+just as famous Roman poet Horace had suggested to do everyday. 
+However, for some reason, the mighty water God Poseidon has not enjoyed 
+his drinks last night as much as he usually does; and threw a lot of spare 
+ice cubes from the drinks into the sea! Thus, the water got extensively colder; 
+to be more specific it is 11.475 Seidons cold now, which is 14 Seidons colder 
+than normally. Thus today swimming is not allowed. Seidon is the temperature 
+entity of the sea. 1 Seidon devided by 1.37 equal 1 Degree Celsius. 
+Print the water temperature in both entities with max two fraction digit 
+with an equation on a normal, ice-cube free day! */
+
+
+void main() {
+double tempSeidon = 25.475;
+double tempCelsius = (tempSeidon/1.37);
+print('${tempSeidon.toStringAsFixed(2)}Seidons) = ${tempCelsius.toStringAsFixed(2)}Celsius');
+
+}
+
+// Tags Coding: Learn doubles, learn strings
+// Tags Story semantics: Ancient Gods, Roman philosophy, 
+// Documentation 
