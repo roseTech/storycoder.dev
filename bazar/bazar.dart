63210ed505d46dc678957f5bcf7d1013a649cd55
roseTech <110260001+roseTech@users.noreply.github.com>
2022-08-25 23:43:52 +0200

    Add files via upload

---
+++ bazar/bazar.dart
@@ -0,0 +1,14 @@
+main() {
+  int elephant = 10;
+  int bicycles = 10;
+  int sunflower = 10;
+  int initialPrice = elephant + bicycles + sunflower;
+  elephant += 3;
+  sunflower -= 5;
+  bicycles = elephant + 2;
+  int finalPrice = elephant + bicycles + sunflower;
+
+  int gainOrLoss = finalPrice - initialPrice;
+  print('in the hend the bazar seller has gained $gainOrLoss \$');
+  print('so his idea to change prices was not smart');
+}
