ff6c492be73b8606859f466f3d1d67cac0d106d8
Darijo <topic.darijo@gmail.com>
2022-08-25 20:24:08 +0200

    move story to dir

---
+++ poseidon/poseidon.dart
@@ -0,0 +1,7 @@
+main() {
+
+void main() {
+double tempSeidon = 25.475;
+double tempCelsius = (tempSeidon/1.37);
+print('${tempSeidon.toStringAsFixed(2)}Seidons) = ${tempCelsius.toStringAsFixed(2)}Celsius');
+}
