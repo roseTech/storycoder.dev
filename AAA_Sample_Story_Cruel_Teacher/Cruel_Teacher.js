a53c80e7afa1ecb988cbeea7e4aa4e83f96a1752
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-08-29 21:22:18 +0200

    Add Cruel_Teacher.js

---
+++ AAA_Sample_Story_Cruel_Teacher/Cruel_Teacher.js
@@ -0,0 +1,8 @@
+function main() {
+    const iterations = 10;
+    const p = 7;
+    const sum = new Array(iterations).fill(0).map((_, i) => (i + 1) * p).reduce((acc, value) => acc + value);
+    console.log(sum);
+}
+
+main()
