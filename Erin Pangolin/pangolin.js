7e52fb065924da1f11d1dfa20e3f4ebde9bb8790
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-08-29 16:32:26 +0200

    Add Erin Pangolin

---
+++ Erin Pangolin/pangolin.js
@@ -0,0 +1,19 @@
+
+const peaks = [1335, 1439, 987, 312, 871, 1461, 1171, 654, 123];
+
+function main() {
+    let steepnessMax = 0;
+    let indexMax = -1;
+
+    for (let i = 0; i < (peaks.length - 1); i++) {
+        const steepness = Math.abs(peaks[i] - peaks[i + 1]);
+        if (steepness > steepnessMax) {
+            steepnessMax = steepness;
+            indexMax = i;
+        }
+    }
+
+    console.log(indexMax);
+}
+
+main();
