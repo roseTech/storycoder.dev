6eb122f948fe8bcd23c0aec7f2a7243db7549c18
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-08-30 11:50:28 +0200

    Poseidon: add Go and Clojure solution

---
+++ AAA_Sample_Story_Poseidons_Ice_Cubes/Poseidons_Ice_Cubes.clj
@@ -0,0 +1,6 @@
+(defn main[]
+  (def tempSeidon (+ 11.475 14.0))
+  (def tempCelsius (/ tempSeidon 1.37))
+  (println (format "%.2f" tempCelsius)))
+
+(main)
---
+++ AAA_Sample_Story_Poseidons_Ice_Cubes/Poseidons_Ice_Cubes.go
@@ -0,0 +1,8 @@
+package main import "fmt" func
+
+main ()
+{
+  var tempSeidon = 11.475 + 14.0;
+  var tempCelsius = tempSeidon / 1.37;
+  fmt.Printf("%.2f", tempCelsius);
+}
