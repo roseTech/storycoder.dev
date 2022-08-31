bdbbed1b385aefbeefff00b39a0d21321a16ee72
roseTech <110260001+roseTech@users.noreply.github.com>
2022-08-31 20:59:56 +0200

    Update Cruel_Teacher.dart

--- AAA_Sample_Story_Cruel_Teacher/Cruel_Teacher.dart
+++ AAA_Sample_Story_Cruel_Teacher/Cruel_Teacher.dart
@@ -1,3 +1,4 @@
+//approach for advanced learners
 void main() {
   const iterations = 10;
   const p = 7;
@@ -5,3 +6,18 @@ void main() {
       List.generate(iterations, (i) => p * (i + 1)).reduce((sum, e) => sum + e);
   print(sum);
 }
+
+
+// appraoch for beginners
+void main() {
+  const iterations = 10;
+  const p = 7;
+  var sum = 0;
+  for (int i = 1; i <= iterations; i++) {
+    final num = p * i;
+    sum += num;
+    print ('$i $num $sum');
+  }
+}
+
+
