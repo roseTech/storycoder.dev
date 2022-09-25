31b99262a340c4e2ddd0ef7dbd650b62d16ef302
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-09-25 12:52:44 +0200

    Ode to a Night In Coder: python

---
+++ Ode_To_A_Night_In_Coder/Ode_To_A_Night_In_Coder.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+bell_rings = 0
+
+for hour in list(range(8, 12 + 1)) + list(range(1, 5 + 1)):
+    for minute in range(0, 60, 15):
+        # print(hour, minute)
+        bell_rings += hour + (minute // 15)
+
+print(bell_rings)
