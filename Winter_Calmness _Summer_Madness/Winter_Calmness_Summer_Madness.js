1f5765e041165edd57be91a7139ef60060347984
najwanan <theunnamedrd@gmail.com>
2022-10-29 18:06:53 -0700

    finalizing js solution

--- Winter_Calmness _Summer_Madness/Winter_Calmness_Summer_Madness.js
+++ Winter_Calmness _Summer_Madness/Winter_Calmness_Summer_Madness.js
@@ -1,37 +1,56 @@
 function carTraffic() {
 
-  let peopleCounter = 0;  
-  for (let i = 0; i <= 360; i++) {
+  let peopleInCar = 0;  
+  for (let i = 1; i <= 360; i++) {
     if (i % 6 === 0) {
-      peopleCounter += 5;
+      peopleInCar += 5;
     } else {
-      peopleCounter++;
+      peopleInCar++;
     }
   }
-  /*i%6===0?peopleCounter +=5: peopleCounter ++   */
+  console.log(peopleInCar)
+  return peopleInCar
 }
 
 
 function busTraffic() {
 
-    let peopleCounter = 0
+  let peopleInBus = 0;
+ 
     for (let i = 0; i <= 360; i += 3) {
      
-        if (i % 15 === 0) {
-            peopleCounter += 12
-        } else {
-            peopleCounter += 2
+        if (i % 15 === 0 && i!=0) {
+          peopleInBus += 12;
+        } else if(i!=0){
+          peopleInBus += 2;
         }
     }
-}
 
+  console.log(peopleInBus);
+  return peopleInBus
+}
 
 
 
 function getNumberOfWalkersInIntervalInHour(intervalLength, peopleAddition) {
-  const intervals = 60 / intervalLength;
-  const numberOfPeopleInInterval = intervalLength * (intervalLength + 1);
-  const allPeople = numberOfPeopleInInterval * interval;
+  let allPeopleOnStreet = 0;
+	const intervals = 360 / intervalLength;
+//360 represents the 360 minutes between noon and 6pm
+for(let interval=1; interval <= intervals; interval++) {
+	for(let minute=1; minute<= intervalLength; minute++ ) {
+          allPeopleOnStreet += minute;
+  }
+} 
+console.log(allPeopleOnStreet);
+return allPeopleOnStreet;
 
-  return allPeople;
 }
+
+let allPeople =
+  getNumberOfWalkersInIntervalInHour(10, 1) + busTraffic() + carTraffic();
+ console.log(allPeople);
+  
+ 
+  
+
+
