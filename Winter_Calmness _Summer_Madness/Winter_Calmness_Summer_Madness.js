e0366139bd2d3fb2b6a19ec9e662bc58b1ffdcd5
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-11-08 21:48:41 +0100

    Winter_Calmness_Summer_Madness: format js

--- Winter_Calmness _Summer_Madness/Winter_Calmness_Summer_Madness.js
+++ Winter_Calmness _Summer_Madness/Winter_Calmness_Summer_Madness.js
@@ -1,6 +1,6 @@
 function carTraffic() {
+  let peopleInCar = 0;
 
-  let peopleInCar = 0;  
   for (let i = 1; i <= 360; i++) {
     if (i % 6 === 0) {
       peopleInCar += 5;
@@ -8,49 +8,39 @@ function carTraffic() {
       peopleInCar++;
     }
   }
-  console.log(peopleInCar)
+
+  console.log('people in car', peopleInCar);
   return peopleInCar
 }
 
-
 function busTraffic() {
-
   let peopleInBus = 0;
- 
-    for (let i = 0; i <= 360; i += 3) {
-     
-        if (i % 15 === 0 && i!=0) {
-          peopleInBus += 12;
-        } else if(i!=0){
-          peopleInBus += 2;
-        }
-    }
 
-  console.log(peopleInBus);
+  for (let i = 0; i <= 360; i += 3) {
+    if (i % 15 === 0 && i != 0) {
+      peopleInBus += 12;
+    } else if (i != 0) {
+      peopleInBus += 2;
+    }
+  }
+
+  console.log('people in bus', peopleInBus);
   return peopleInBus
 }
 
-
-
 function getNumberOfWalkersInIntervalInHour(intervalLength, peopleAddition) {
   let allPeopleOnStreet = 0;
-	const intervals = 360 / intervalLength;
-//360 represents the 360 minutes between noon and 6pm
-for(let interval=1; interval <= intervals; interval++) {
-	for(let minute=1; minute<= intervalLength; minute++ ) {
-          allPeopleOnStreet += minute;
+  const intervals = 360 / intervalLength;
+  //360 represents the 360 minutes between noon and 6pm
+  for (let interval = 1; interval <= intervals; interval++) {
+    for (let minute = 1; minute <= intervalLength; minute++) {
+      allPeopleOnStreet += minute;
+    }
   }
-} 
-console.log(allPeopleOnStreet);
-return allPeopleOnStreet;
 
+  console.log('people on street', allPeopleOnStreet);
+  return allPeopleOnStreet;
 }
 
-let allPeople =
-  getNumberOfWalkersInIntervalInHour(10, 1) + busTraffic() + carTraffic();
- console.log(allPeople);
-  
- 
-  
-
-
+const allPeople = getNumberOfWalkersInIntervalInHour(10, 1) + busTraffic() + carTraffic();
+console.log('all people', allPeople);
