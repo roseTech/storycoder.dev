9521c67d385741f4554e72f55aac349575d9d7ad
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-12-16 19:21:49 +0100

    Snow Everywhere: javascript solution

---
+++ Snow_Everywhere/Snow_Everywhere.js
@@ -0,0 +1,20 @@
+
+function areaEllipse(a, b) {
+    return a * b * Math.PI;
+}
+
+function areaRectangle(width, length) {
+    return width * length;
+}
+
+function areaHexagon(side) {
+    return (3.0 * Math.sqrt(3) * Math.pow(side, 2.0)) / 2.0;
+}
+
+function areaCircle(radius) {
+    return areaEllipse(radius, radius);
+}
+
+const fabricSquareMeter = areaEllipse(1.1, 2.4) + areaRectangle(2.1, 0.8) + areaHexagon(0.9) + areaCircle(0.5);
+
+console.log(Math.ceil(fabricSquareMeter));
