dbcddb8a0286354efb929d783887a0d952a6a4f4
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-12-24 15:21:37 +0100

    Format Code

--- Antheias_Garden/Antheias_Garden.kt
+++ Antheias_Garden/Antheias_Garden.kt
@@ -1,25 +1,20 @@
-package com.eaglewarrior.antheiaskotlinsolution
+fun main() {
 
-class LiliesSolution {
-    fun main() {
+    val numbOfLily = 1.0
+    val daysUntilFullMoon = 29
+    var totalNumOfLily = numbOfLily
 
-        val numbOfLily = 1.0
-        val daysUntilFullMoon = 29
-        var totalNumOfLily = numbOfLily
+    for (i in 1..daysUntilFullMoon) {
+        println("Day ${i}, There are ${totalNumOfLily} Lilies")
+        totalNumOfLily = totalNumOfLily * 2
+    }
 
-        for (i in 1..daysUntilFullMoon) {
-            println("Day ${i}, There are ${totalNumOfLily} Lilies")
-            totalNumOfLily = totalNumOfLily * 2
-        }
+    var lilyNum = 1
+    var daysNum = 1
 
-
-        var lilyNum = 1
-        var daysNum = 1
-
-        while (daysNum <= 29) {
-            println("On day ${daysNum}, there should be ${lilyNum} Lilies in the pond.")
-            lilyNum = lilyNum * 2
-            daysNum++
-        }
+    while (daysNum <= 29) {
+        println("On day ${daysNum}, there should be ${lilyNum} Lilies in the pond.")
+        lilyNum = lilyNum * 2
+        daysNum++
     }
 }
