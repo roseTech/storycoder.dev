c93ae523b737bf0e4d50310fb1bdb51d60781c3e
Thomas Stauffer <thomasstauffer@gmx.ch>
2023-02-15 19:06:26 +0100

    Johanns Harp: revert old ruby solution as it prints out the real solution

--- Johanns_Harp/Johanns_Harp.rb
+++ Johanns_Harp/Johanns_Harp.rb
@@ -1,16 +1,2 @@
-# Johann's Harp solution in Ruby
-# Rubberduck.tech + human-edited
-# 2023-02-14
-
-# Factorzation function
-# start = starting number; factor; iterate = loop count
-def factorize(start, factor, iterate)
-  result = start
-  iterate.times do
-    result *= factor
-  end
-  return result
-end
-
-# Round the result to 6 decimal places
-puts format("%.6f", factorize(1.0, 2.0 ** (1.0 / 12.0), 12))
+factor = 2.0 ** (1.0 / 12.0)
+puts "#{(factor - 1) * 100}%"
